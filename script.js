182700a6f0699a77d5aa1f82847900722e06566a
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-10-29 17:45:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function repeat() {
-    const holder = document.querySelector('.holder');
-    holder.style.display = 'grid';
-    holder.style.gridTemplateColumns = '100px 100px';
-}
-repeat()
+// function repeat() {
+//     const holder = document.querySelector('.holder');
+//     holder.style.display = 'grid';
+//     holder.style.gridTemplateColumns = '100px 100px';
+// }
+// repeat()
